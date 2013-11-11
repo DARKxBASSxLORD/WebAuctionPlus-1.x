4d9e26fec07064f962b04cc8011ea93602c267f8
Pete Lorenzo <lorenzo@poixson.com>
2013-11-11 09:44:36 -0500

    cleaned up and finished some of the main WebAuctionPlus class single instance handling, Vault, and LinkManager loading

--- java/src/com/webauctionplus/WebAuctionPlus.java
+++ java/src/com/webauctionplus/WebAuctionPlus.java
@@ -1,38 +1,54 @@
 package com.webauctionplus;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.poixson.webxbukkit.PluginVersion;
+import com.poixson.webxbukkit.Vault;
+import com.poixson.webxbukkit.webLink.LinkManager;
 import com.poixson.webxbukkit.webSettings.SettingsManager;
 
 
 public class WebAuctionPlus extends JavaPlugin {
 
-	private static volatile WebAuctionPlus plugin = null;
+	private static volatile WebAuctionPlus instance = null;
 	private static final Object lock = new Object();
+//	private static volatile xLog log = null;
 
 	// objects
 	private volatile waConfig config = null;
+@SuppressWarnings("unused")
 	private volatile SettingsManager settings = null;
 	private volatile PluginVersion version = null;
 	private final waLanguage lang = new waLanguage();
+	// web link
+@SuppressWarnings("unused")
+	private volatile LinkManager link = null;
 
 	// database key
 	private volatile String dbKey = null;
 
-	// run state
+	// null=unloaded false=failed true=loaded
 	private static volatile Boolean isOk = null;
-	private static volatile boolean debug = false;
 
 
+	// get instance
+	public static WebAuctionPlus get() {
+		return instance;
+	}
 	public WebAuctionPlus() {
 		super();
 		synchronized(lock) {
-			if(plugin != null) throw new RuntimeException("Plugin plugin already loaded?!");
-			plugin = this;
+			if(instance != null) throw new RuntimeException("Plugin already loaded?!");
+			instance = this;
 		}
 	}
+	public static boolean isOk() {
+		return isOk;
+	}
+
+
 	@Override
 	public void onEnable() {
 		synchronized(lock) {
@@ -43,12 +59,19 @@ public class WebAuctionPlus extends JavaPlugin {
 				return;
 			}
 			isOk = false;
+			if(instance == null)
+				instance = this;
+		}
+		// load vault (required)
+		if(Vault.getEconomy() == null) {
+			System.out.println("Economy plugin not found!");
+			Bukkit.getPluginManager().disablePlugin(this);
+			return;
 		}
 
 		// plugin version
 		version = PluginVersion.get(this);
 		version.update();
-
 		// config.yml
 		config = new waConfig(this);
 		// connect to db
@@ -57,6 +80,15 @@ public class WebAuctionPlus extends JavaPlugin {
 		settings = SettingsManager.get(dbKey);
 		// language
 		lang.load(this, "en");
+		// web link
+		link = LinkManager.get(dbKey);
+		LinkManager.start();
+
+
+
+
+
+
 
 		isOk = true;
 	}
@@ -65,21 +97,12 @@ public class WebAuctionPlus extends JavaPlugin {
 		isOk = false;
 		// stop schedulers
 		try {
-			getServer().getScheduler().cancelTasks(this);
+			Bukkit.getScheduler().cancelTasks(this);
 		} catch (Exception ignore) {}
 
 
 
-
 		isOk = null;
-	}
-
-
-	public static boolean isOk() {
-		return isOk;
-	}
-	public static boolean isDebug() {
-		return debug;
 	}
 
 
